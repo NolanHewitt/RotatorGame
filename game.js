b1159a6536a524d63fca7d6dd978ce9c72d47799
Nolan Hewitt <nolanhewitt@aol.com>
2019-12-04 23:39:05 -0600

    test

--- game.js
+++ game.js
@@ -1,14 +1,19 @@
-let angle = 0;
+// let angle = 0;
 
 
-    setInterval(function(){
-        $('#dot').css('transform','rotate(' + angle + 'deg)');
-     }, 17);
+//     setInterval(function(){
+//         $('#dot').css('transform','rotate(' + angle + 'deg)');
+//      }, 17);
 
 
 
-window.addEventListener('deviceorientation', function(e) {
-    console.log( e.webkitCompassHeading );
-    angle = e.webkitCompassHeading;
-    document.getElementById("data").innerHTML=("Degrees: " + Math.floor(angle) + "°");
-}, false);
+// window.addEventListener('deviceorientation', function(e) {
+//     console.log( e.webkitCompassHeading );
+//     angle = e.webkitCompassHeading;
+//     document.getElementById("data").innerHTML=("Degrees: " + Math.floor(angle) + "°");
+// }, false);
+
+
+navigator.geolocation.watchPosition((data) => {
+    console.log(data);
+});
